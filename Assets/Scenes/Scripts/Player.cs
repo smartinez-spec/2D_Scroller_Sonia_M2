46a3f54c295dbdbf0e03f3f95353af7a2b06661d
Sonia Martinez <s.martinez@ensi.lan>
2026-04-24 16:32:26 +0200

    mouvement du personnage

---
+++ Assets/Scenes/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed;
+    public float jumpingPower;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    float horizontal;
+    SpriteRenderer sr;
+
+    private void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+     private void FixedUpdate()
+     {
+         rb.linearVelocity = new Vector2(horizontal * speed, rb.linearVelocity.y);
+     }
+     
+    public void Move(InputAction.CallbackContext context)
+    {
+        horizontal = context.ReadValue<Vector2>().x;
+        sr.flipX = horizontal < 0 ? true : false;
+    }
+    
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+    }
+    bool IsGrounded()
+    {
+        return Physics2D.OverlapCapsule(groundCheck.position, new Vector2(1f, .1f), CapsuleDirection2D.Horizontal, 0,
+            groundLayer);
+    }
+}
